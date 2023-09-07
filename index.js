801722989a1d1b7e477ce7af487831741f360d27
“Elodie <mail.elodielongo@gmail.com>
2023-09-08 00:06:40 +0200

    draft 1

---
+++ index.js
@@ -0,0 +1,36 @@
+
+function runQuiz() {
+ console.log("here")   
+}
+
+var questionsArr = [
+    {
+      question1: 'JavaScript is the best language',
+      answer: true
+    },
+    {
+        question2: 'JavaScript is the best language',
+        answer: true
+      },
+      {
+        question3: 'JavaScript is the best language',
+        answer: true
+      },
+      {
+        question4: 'JavaScript is the best language',
+        answer: true
+      },
+      {
+        question5: 'JavaScript is the best language',
+        answer: true
+      },
+    
+]
+  
+
+for(var i=0; i < questionsArr.lenght; i++) {
+    runQuiz()
+}
+
+
+
