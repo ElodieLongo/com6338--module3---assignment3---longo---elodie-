bd931619315375d865ea6ca22174a52759044289
“Elodie <mail.elodielongo@gmail.com>
2023-09-15 01:17:15 +0200

    Assignment-v2

--- index.js
+++ index.js
@@ -1,47 +1,42 @@
- 
-
 var questionsArr = [
   {
     question: 'Rome is the capital of Italy',
     answer: true
   },
   {
-      question: 'Paris is the capital of Spain',
-      answer: false
-    },
-    {
-      question: 'Germany is in southern Europe',
-      answer: false
-    },
-    {
-      question: 'The Alps are at the border between France and Spain',
-      answer: false
-    },
-    {
-      question: 'Sweden is the European country with the most islands',
-      answer: true
-    }  
+    question: 'Paris is the capital of Spain',
+    answer: false
+  },
+  {
+    question: 'Germany is in southern Europe',
+    answer: false
+  },
+  {
+    question: 'The Alps are at the border between France and Spain',
+    answer: false
+  },
+  {
+    question: 'Sweden is the European country with the most islands',
+    answer: true
+  }
 ];
 
-
- function runQuiz() {
+function runQuiz() {
   let score = 0;
 
-  for (let i  = 0; i < questionsArr.length; i++) {
-
+  for (let i = 0; i < questionsArr.length; i++) {
     let userAnswer = confirm(questionsArr[i].question);
     if (userAnswer === questionsArr[i].answer) {
-      score++;  
-     }
+      score++;
     }
-
-let percentageScore = (score / questionsArr.length) * 100;
-window.alert(`Your score is ${percentageScore.toFixed()}%`);
   }
-  
-function showResults(){
-let score = (percentageScore/questionsArr) * 100;
 
+  let percentageScore = (score / questionsArr.length) * 100;
+  window.alert(`Your score is ${percentageScore.toFixed()}%`);
 }
 
-runQuiz ()
+function showResults() {
+  // This function appears to be incomplete or unnecessary for your current code.
+}
+
+runQuiz();
